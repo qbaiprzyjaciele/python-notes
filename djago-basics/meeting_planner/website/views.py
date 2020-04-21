d96b87d051c3ba9e384010206b469cc69cbddb1c
Jakub Smorąg <qbaiprzyjaciele@gmail.com>
2020-04-21 18:05:18 +0200

    learning django

--- djago-basics/meeting_planner/website/views.py
+++ djago-basics/meeting_planner/website/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from datetime import datetime
 from django.http import HttpResponse
+
 
 def welcome(request):
     return HttpResponse("Welcome to the Meeting Planner")
+
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
+
+def about(request):
+    return HttpResponse("Some info about me")
