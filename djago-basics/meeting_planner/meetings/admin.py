a4d46172f1f3acdcb88f5f8da6b4e331db6f67c3
Jakub Smorąg <qbaiprzyjaciele@gmail.com>
2020-05-01 10:30:20 +0000

    more of django learning

--- djago-basics/meeting_planner/meetings/admin.py
+++ djago-basics/meeting_planner/meetings/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 from .models import Meeting
+from .models import Agenda
+from .models import Room
 
 admin.site.register(Meeting)
+admin.site.register(Agenda)
+admin.site.register(Room)
 
--- djago-basics/meeting_planner/meetings/models.py
+++ djago-basics/meeting_planner/meetings/models.py
@@ -1,7 +1,34 @@
+from datetime import time
 from django.db import models
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+    floor_number = models.IntegerField()
+    room_number = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} at {self.floor_number} on {self.room_number}"
 
 
 # Create your models here.
 class Meeting(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
+    start_time = models.TimeField(default=time(9))
+    duration = models.IntegerField(default=1)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} at {self.start_time} on {self.date}"
+
+
+class Agenda(models.Model):
+    title = models.CharField(max_length=500)
+    ordinal_num = models.IntegerField()
+    start_time = models.TimeField(default=time(9))
+    end_time = models.TimeField(default=time(9))
+    meeting = models.ForeignKey(Meeting, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f"{self.ordinal_num}. {self.start_time} - {self.end_time} {self.title}"
